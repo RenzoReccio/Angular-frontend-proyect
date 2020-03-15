a1d6d7141442b69bf603b3a4ec69692782a5318e
RenzoReccio <reccio_2001@hotmail.com>
2020-03-15 17:10:13 -0500

    going on!

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id');
+
   }
 
 }
